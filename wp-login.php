4eedbcdda21e9b8a4e01591b449cc89826936d4a
jsbryce1 <jedi1991@hotmail.com>
2019-01-14 18:27:15 -0700

    name change to asap and favicon added

--- wp-login.php
+++ wp-login.php
@@ -7,22 +7,26 @@
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Log In &lsaquo; ASAP WORKS &#8212; WordPress</title>
+	<title>Log In &lsaquo; asap  &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://jsbryce1.github.io/thera/wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://jsbryce1.github.io/thera/wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://jsbryce1.github.io/thera/wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://jsbryce1.github.io/thera/wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://jsbryce1.github.io/thera/wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://jsbryce1.github.io/asap/wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://jsbryce1.github.io/asap/wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://jsbryce1.github.io/asap/wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://jsbryce1.github.io/asap/wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://jsbryce1.github.io/asap/wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-		</head>
+	<link rel="icon" href="https://jsbryce1.github.io/asap/wp-content/uploads/2019/01/aa-symbol-clipart-1-150x150.png" sizes="32x32" />
+<link rel="icon" href="https://jsbryce1.github.io/asap/wp-content/uploads/2019/01/aa-symbol-clipart-1-230x230.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://jsbryce1.github.io/asap/wp-content/uploads/2019/01/aa-symbol-clipart-1-230x230.png" />
+<meta name="msapplication-TileImage" content="https://jsbryce1.github.io/asap/wp-content/uploads/2019/01/aa-symbol-clipart-1.png" />
+	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="https://jsbryce1.github.io/thera/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://jsbryce1.github.io/asap/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -34,13 +38,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="https://jsbryce1.github.io/thera/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://jsbryce1.github.io/asap/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="https://jsbryce1.github.io/thera/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="https://jsbryce1.github.io/asap/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -57,7 +61,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://jsbryce1.github.io/thera/">&larr; Back to ASAP WORKS</a></p>
+	<p id="backtoblog"><a href="https://jsbryce1.github.io/asap/">&larr; Back to asap </a></p>
 		
 	</div>
 
